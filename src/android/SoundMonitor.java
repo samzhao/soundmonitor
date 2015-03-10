d14c5b860e084ef4050c6b7311e7767d3d6e5626
Sam Zhao <zhao6518@gmail.com>
2015-03-09 22:49:39 -0700

    change again

--- src/android/SoundMonitor.java
+++ src/android/SoundMonitor.java
@@ -185,7 +185,7 @@ public class SoundMonitor extends CordovaPlugin {
         // return max;
 
         if (mRecorder != null) {
-            return (mRecorder.getMaxAmplitude()/2700.0);
+            return (20 * Math.log10(mRecorder.getMaxAmplitude()/32767));
         } else {
             return 0;
         }
